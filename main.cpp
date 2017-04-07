e1a73a313fa847324dd22a40f3785632ef5f2c7f
Antoine Rabenandrasana <ariary9.2@hotmail.fr>
2017-04-07 18:29:51 +0200

    Enregistrement des resultats dans un .txt

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "./include/grapheBiparti.h"
 #include "./include/sommet.h"
-
+#include <fstream>
 
 using namespace std;
 
@@ -9,10 +9,40 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {	
-	GrapheBiparti g1(1000,1000);	
-	g1.generate(0.005);
-	cout<<g1.HopcroftKarp()<<endl;
 
+
+	ofstream result("result.txt", ios::out | ios::trunc);  //déclaration du flux et ouverture du fichier
+
+        
+
+	if(result)
+	{
+		double p=0.001;
+
+		while(p<0.06){
+			result<<"p=";
+			result<< p ;
+			result<<":"<<endl;
+			for (int i = 0; i < 100; ++i) //on fait 1000 tests par valeur
+			{
+				GrapheBiparti g1(500,500);
+				g1.generate(p);
+				result<<g1.HopcroftKarp()<<endl;
+			}
+
+			p+=0.005;
+			cout << p << endl; //pour savoir où j'en suis
+		}
+
+		result.close();  // on referme le fichier
+	}
+	else
+		cerr << "Erreur à l'ouverture du fichier!" << endl;
+
+
+
+
+//O.015 0.02
 
 	return 0;
 }
